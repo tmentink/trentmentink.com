056a4827294e318d673784057795958476584068
Trent Mentink <trentmentink@gmail.com>
2018-03-23 14:54:22 -0700

    changed preloader run to use velocity

--- src/js/main/preloader.js
+++ src/js/main/preloader.js
@@ -22,9 +22,8 @@ var Preloader = ((Preloader) => {
   }
 
   const FADE_DURATION   = 1500
-  const LOAD_DURATION   = 1500
   const LOAD_INTERVAL   = 50
-  const RUN_DURATION    = 2500
+  const RUN_DURATION    = 2750
 
 
   // ----------------------------------------------------------------------
@@ -38,16 +37,25 @@ var Preloader = ((Preloader) => {
 
     setTimeout(() => {
       $cache(Selector.PRELOADER).addClass(ClassName.HIDE)
+      $cache(Selector.PRELOADER).removeClass(ClassName.ACTIVE)
     }, FADE_DURATION)
+  }
+
+  Preloader.run = function() {
+    const easing = BP.max_sm.matches
+      ? [0.680, -1.2, 0.265, 1.4]
+      : [0.680, -0.65, 0.265, 1.4]
+
+    $cache(Selector.PRELOADER_CONTAINER).velocity({left: "110%"}, RUN_DURATION, easing)
   }
 
   Preloader.stop = function() {
     $cache(Selector.PRELOADER_TEXT).addClass(ClassName.TEXT_FADE)
-    $cache(Selector.PRELOADER_CONTAINER).addClass(ClassName.CONTAINER_RUN)
+    Preloader.run()
 
     setTimeout(() => {
       Preloader.fadeOut()
-    }, RUN_DURATION)
+    }, RUN_DURATION - 500)
   }
 
 
