41bb39151f3b9742ae6ad05e04db4e0dee472997
Trent Mentink <trentmentink@gmail.com>
2018-04-03 08:42:28 -0700

    added insertAt to jQuery

--- src/js/util/jquery-extensions.js
+++ src/js/util/jquery-extensions.js
@@ -2,6 +2,47 @@
 !(() => {
   "use strict"
 
+  // ----------------------------------------------------------------------
+  // Easings
+  // ----------------------------------------------------------------------
+
+  $.easing.easeInOutExpo = function(x) {
+    return x === 0 ? 0 : x === 1 ? 1 : x < 0.5
+      ? Math.pow(2, 20 * x - 10) / 2
+      : (2 - Math.pow(2, -20 * x + 10)) / 2
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Insert At
+  // ----------------------------------------------------------------------
+
+  $.fn.insertAt = function(index, $parent) {
+    return this.each(function() {
+      index === 0
+        ? $parent.prepend(this)
+        : $parent.children().eq(index - 1).after(this)
+    })
+  }
+
+
+  // ----------------------------------------------------------------------
+  // In View
+  // ----------------------------------------------------------------------
+
+  $.fn.inView = function(offset) {
+    if (!this.length) return false
+    const rect = this.get(0).getBoundingClientRect()
+    offset     = offset || 0
+
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top + offset <= document.documentElement.clientHeight &&
+      rect.left<= document.documentElement.clientWidth
+    )
+  }
+
 
   // ----------------------------------------------------------------------
   // Selector Cache
@@ -25,35 +66,6 @@
     }
 
     return SelectorCache[cacheKey]
-  }
-
-
-  // ----------------------------------------------------------------------
-  // In View
-  // ----------------------------------------------------------------------
-
-  $.fn.inView = function(offset) {
-    if (!this.length) return false
-    const rect = this.get(0).getBoundingClientRect()
-    offset     = offset || 0
-
-    return (
-      rect.bottom >= 0 &&
-      rect.right >= 0 &&
-      rect.top + offset <= document.documentElement.clientHeight &&
-      rect.left<= document.documentElement.clientWidth
-    )
-  }
-
-
-  // ----------------------------------------------------------------------
-  // Easings
-  // ----------------------------------------------------------------------
-
-  $.easing.easeInOutExpo = function(x) {
-    return x === 0 ? 0 : x === 1 ? 1 : x < 0.5
-      ? Math.pow(2, 20 * x - 10) / 2
-      : (2 - Math.pow(2, -20 * x + 10)) / 2
   }
 
 })()
